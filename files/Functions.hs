4b6f82d73f9063ea301e1b015333c56e7719b782
Max Hammond <max.hammond21@gmail.com>
2022-12-03 10:48:27 +1100

    functions

--- files/Functions.hs
+++ files/Functions.hs
@@ -60,3 +60,13 @@ zipWidth' f (x:xs) (y:ys) = f x y : zipWidth' f xs ys
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f = g
     where g x y = f y x
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
